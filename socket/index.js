30fc2fa636c5e8ec422d03b067ffb74221414a70
chriszhu <1633711653@qq.com>
2021-04-25 17:32:21 +0800

    增加在线用户的socket接口

--- socket/index.js
+++ socket/index.js
@@ -1,4 +1,5 @@
 const IO = require('socket.io')
+const { verifyToken } = require('../utils/jwt')
 
 const creatSocket = (server) => {
   const io = IO(server, {
@@ -6,9 +7,33 @@ const creatSocket = (server) => {
       origin: '*'
     }
   })
+
+  let users = []
+
   io.on('connection', (socket) => {
-    socket.send('hi', 'server api')
-    console.log('已连接')
+    // socket.send('hi', 'server api')
+    // console.log('已连接')
+
+    let curUser = {}
+
+    socket.on('login', (data) => {
+      const result = verifyToken(data.token)
+      if (result) {
+        const { _id } = result // 用户 token --> _id
+        if (users.findIndex((el) => el._id === _id) === -1) {
+          // 确保当前用户唯一
+          const tempUser = { _id, ...data.userInfo }
+          users.push({ ...tempUser })
+          curUser = { ...tempUser }
+        }
+      }
+    })
+
+    socket.on('users', () => {
+      const arr = users.filter((el) => el._id !== curUser._id)
+      socket.emit('users', arr)
+    })
+
     // console.log(Object.keys(socket));
     let handshake = socket.handshake
     // console.log(handshake)
--- utils/jwt.js
+++ utils/jwt.js
@@ -5,19 +5,26 @@ const expiresIn = 1000 * 60 * 60 * 24 * 7 // 过期时间
 module.exports = {
   key: secert,
   expiresIn,
-  //生成token
+  /**
+   * 加密，返回token
+   * @param {any} payload 加密载体 payload
+   * @returns 返回一个token值
+   */
   generateToken: (payload) => jwt.sign(payload, secert, { expiresIn }),
-  // 校验token
-  // verifyToken: (token) => {
-  //   let result;
-  //   try {
-  //     jwt.verify(token, secert, (err, data) => {
-  //       if (err) result = 'error'
-  //       else result = data
-  //     })
-  //   } catch (err) {
-  //     result = 'error'
-  //   }
-  //   return result
-  // }
+  /**
+   * 解析token，返回解析后加密的值。解析失败，返回 null
+   * @param {string} token token值
+   * @returns 解析结果 { _id: string, iat: number, exp: number }
+   */
+  verifyToken: (token) => {
+    let result = null
+    try {
+      jwt.verify(token, secert, (err, data) => {
+        if (!err) result = data
+      })
+    } catch (err) {
+      console.log(err)
+    }
+    return result
+  }
 }
